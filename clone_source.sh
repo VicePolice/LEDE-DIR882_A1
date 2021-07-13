cd4549ff044a60ad323c902c22d815579405b66a
blueveryday <29169337+blueveryday@users.noreply.github.com>
2021-07-13 21:41:30 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone "https://github.com/immortalwrt/immortalwrt.git"
+git clone "https://github.com/blueveryday/immortalwrt.git"
 pushd "immortalwrt"; ./scripts/feeds update -a -f; ./scripts/feeds install -a -f; popd
