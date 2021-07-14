c84519df9fb0faddeca4d625ada405e9baefda2e
blueveryday <29169337+blueveryday@users.noreply.github.com>
2021-07-15 07:01:08 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone "https://github.com/blueveryday/immortalwrt.git"
-pushd "immortalwrt"; ./scripts/feeds update -a -f; ./scripts/feeds install -a -f; popd
+git clone "https://github.com/coolsnowwolf/lede.git"
+pushd "lede"; ./scripts/feeds update -a -f; ./scripts/feeds install -a -f; popd
